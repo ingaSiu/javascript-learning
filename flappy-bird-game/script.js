a7647549e40ab9bfba3b8c75c4a9b695b800f0ad
ingaSiu <ingasiud@gmail.com>
2023-04-23 20:37:45 +0300

    added var delta + comment

--- flappy-bird-game/script.js
+++ flappy-bird-game/script.js
@@ -1,8 +1,23 @@
+import { updateBird } from './bird.js';
+
 document.addEventListener('keypress', handleStart, { once: true });
 const title = document.querySelector('[data-title]');
 
+let lastTime;
+
 function updateLoop(time) {
-  console.log(time);
+  // skip the first render and start rendering after this
+  if (lastTime === null) {
+    lastTime = time;
+    window.requestAnimationFrame(updateLoop);
+    return;
+  }
+  // delta is a variable we can use to make sure we always move
+  // according to how long since its been the last frame
+  const delta = time - lastTime;
+  updateBird(delta);
+  lastTime = time;
+  window.requestAnimationFrame(updateLoop);
 }
 
 function handleStart() {
