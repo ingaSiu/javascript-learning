885c473c24b6a2527d8521b1b8e4b7f4babd0076
ingaSiu <ingasiud@gmail.com>
2023-04-23 19:19:59 +0300

    script started

---
+++ flappy-bird-game/script.js
@@ -0,0 +1,13 @@
+document.addEventListener('keypress', handleStart, { once: true });
+const title = document.querySelector('[data-title]');
+
+function updateLoop(time) {
+  console.log(time);
+}
+
+function handleStart() {
+  title.classList.add('hide');
+  window.requestAnimationFrame(updateLoop);
+}
+
+function handleLose() {}
