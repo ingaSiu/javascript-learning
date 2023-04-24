cd9861f0fa2121aba51a5ef856107adb0acd72e1
ingaSiu <ingasiud@gmail.com>
2023-04-24 09:40:09 +0300

    added losing condition, pipes

--- flappy-bird-game/script.js
+++ flappy-bird-game/script.js
@@ -1,13 +1,16 @@
-import { updateBird } from './bird.js';
+import { getBirdRect, setupBird, updateBird } from './bird.js';
+
+import { updatePipes } from './pipe.js';
 
 document.addEventListener('keypress', handleStart, { once: true });
 const title = document.querySelector('[data-title]');
+const subtitle = document.querySelector('[data-subtitle]');
 
 let lastTime;
 
 function updateLoop(time) {
   // skip the first render and start rendering after this
-  if (lastTime === null) {
+  if (lastTime == null) {
     lastTime = time;
     window.requestAnimationFrame(updateLoop);
     return;
@@ -16,13 +19,32 @@ function updateLoop(time) {
   // according to how long since its been the last frame
   const delta = time - lastTime;
   updateBird(delta);
+  updatePipes(delta);
+  if (checkLose()) {
+    return handleLose();
+  }
   lastTime = time;
   window.requestAnimationFrame(updateLoop);
 }
 
+function checkLose() {
+  const birdRect = getBirdRect();
+
+  const outsideWorld = birdRect.top < 0 || birdRect.bottom > window.innerHeight;
+  return outsideWorld;
+}
 function handleStart() {
   title.classList.add('hide');
+  setupBird();
+  lastTime = null;
   window.requestAnimationFrame(updateLoop);
 }
 
-function handleLose() {}
+function handleLose() {
+  setTimeout(() => {
+    title.classList.remove('hide');
+    subtitle.classList.remove('hide');
+    subtitle.textContent = '0 pipes';
+    document.addEventListener('keypress', handleStart, { once: true });
+  }, 100);
+}
