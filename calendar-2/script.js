bb9e0216db509a7b158c86515a31331735644cfe
ingaSiu <ingasiud@gmail.com>
2024-04-30 14:33:00 +0300

    add current month days to calendar

--- calendar-2/script.js
+++ calendar-2/script.js
@@ -19,7 +19,7 @@ const calendar = document.querySelector('.calendar'),
 
 let today = new Date();
 let activeDay;
-let month = today.getFullMonth();
+let month = today.getMonth();
 let year = today.getFullYear();
 
 const months = [
@@ -36,3 +36,32 @@ const months = [
   'November',
   'December',
 ];
+
+function initCalendar() {
+  const firstDay = new Date(year, month, 1);
+  const lastDay = new Date(year, month + 1, 0);
+  const prevLastDay = new Date(year, month, 0);
+  const prevDays = prevLastDay.getDate();
+  const lastDate = lastDay.getDate();
+  const day = firstDay.getDay();
+  const nextDays = 7 - lastDay.getDay() - 1;
+
+  date.innerHTML = months[month] + ' ' + year;
+
+  let days = '';
+
+  for (let x = day; x > 0; x--) {
+    days = `<div class= "day prev-date"> ${prevDays - x + 1}</div> `;
+  }
+
+  for (let i = 1; i < lastDate; i++) {
+    if (i === new Date().getDate() && year === new Date().getFullYear() && month === new Date().getMonth()) {
+      days += `<div class = "day today">${i}</div> `;
+    } else {
+      days += `<div class = "day">${i}</div> `;
+    }
+  }
+  daysContainer.innerHTML = days;
+}
+
+initCalendar();
