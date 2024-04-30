3942fe0074cd665aba21c67d7a3bb50a6caf3843
ingaSiu <ingasiud@gmail.com>
2024-04-30 13:58:19 +0300

    add all variables

---
+++ calendar-2/script.js
@@ -0,0 +1,38 @@
+const calendar = document.querySelector('.calendar'),
+  date = document.querySelector('.date'),
+  daysContainer = document.querySelector('.days'),
+  prev = document.querySelector('.prev'),
+  next = document.querySelector('.next'),
+  todayBtn = document.querySelector('.today-btn'),
+  gotoBtn = document.querySelector('.goto-btn'),
+  dateInput = document.querySelector('.date-input'),
+  eventDay = document.querySelector('.event-day'),
+  eventDate = document.querySelector('.event-date'),
+  eventsContainer = document.querySelector('.events'),
+  addEventBtn = document.querySelector('.add-event'),
+  addEventWrapper = document.querySelector('.add-event-wrapper '),
+  addEventCloseBtn = document.querySelector('.close '),
+  addEventTitle = document.querySelector('.event-name '),
+  addEventFrom = document.querySelector('.event-time-from '),
+  addEventTo = document.querySelector('.event-time-to '),
+  addEventSubmit = document.querySelector('.add-event-btn ');
+
+let today = new Date();
+let activeDay;
+let month = today.getFullMonth();
+let year = today.getFullYear();
+
+const months = [
+  'January',
+  'February',
+  'March',
+  'April',
+  'May',
+  'June',
+  'July',
+  'August',
+  'September',
+  'October',
+  'November',
+  'December',
+];
