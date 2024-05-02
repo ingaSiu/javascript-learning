61afd5d48c995ba9410c5a9a67663988f064e1a4
ingaSiu <ingasiud@gmail.com>
2024-05-02 11:50:55 +0300

    add today btn, date input funtionality

--- calendar-2/script.js
+++ calendar-2/script.js
@@ -90,3 +90,50 @@ function nextMonth() {
 
 prev.addEventListener('click', prevMonth);
 next.addEventListener('click', nextMonth);
+
+todayBtn.addEventListener('click', () => {
+  today = new Date();
+  month = today.getMonth();
+  year = today.getFullYear();
+  initCalendar();
+});
+
+dateInput.addEventListener('input', (e) => {
+  //allow only numbers, remove anything else
+  dateInput.value = dateInput.value.replace(/[^0-9/]/g, '');
+
+  if (dateInput.value.length === 2) {
+    // if two numbers are entered, add a slash between
+    dateInput.value += '/';
+  }
+
+  if (dateInput.value.length > 7) {
+    // dont allow more than 7 characters
+    dateInput.value = dateInput.value.slice(0, 7);
+  }
+
+  if (e.inputType === 'deleteContentBackward') {
+    if (dateInput.value.length === 3) {
+      dateInput.value = dateInput.value.slice(0, 2);
+    }
+  }
+});
+
+gotoBtn.addEventListener('click', gotoDate);
+//function to go to entered date
+
+function gotoDate() {
+  const dateArr = dateInput.value.split('/');
+
+  if (dateArr.length === 2) {
+    //date validation
+    if (dateArr[0] > 0 && dateArr[0] < 13 && dateArr[1].length === 4) {
+      month = dateArr[0] - 1;
+      year = dateArr[1];
+      initCalendar();
+      return;
+    }
+  }
+  // if date is invalid
+  alert('Invalid date');
+}
